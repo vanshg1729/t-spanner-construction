924344d268029b54b69da7c707aae3deb1fa48bc
fine-man <spacetime299@gmail.com>
2023-04-21 18:08:21 +0530

    added code for 3-spanner as implemented in paper

---
+++ 3-spanner.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr first
+#define sc second
+#define INF 1e9
+typedef pair<int, int> pii;
+
+const int maxn = 50000;
+
+int choose_node(int n) {
+    int x = rand() % n;
+    int sqrt_n = sqrt(n);
+
+    if (x <= sqrt_n) return 1;
+
+    return 0;
+}
+
+int main() {
+    set<pii> adj[maxn] = {};
+    int cluster[maxn + 10] = {}; // tells the cluster to which node i belongs to
+    vector<int> cluster_centers = {};
+    int cluster_count = 0;
+
+    vector<tuple<int, int, int>> spanner_edges = {};
+
+    int n, m; cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].insert({v, w});
+        adj[v].insert({u, w});
+    }
+
+    // Phase 1 : Cluster formation
+
+    // choosing cluster center nodes randomly with (1/sqrt(n)) probability
+    for (int i = 1; i <= n; i++) {
+        if (choose_node(n)) {
+            cluster[i] = i;
+            cluster_count++;
+            cluster_centers.push_back(i);
+        }
+    }
+
+    // connecting non-sampled vertex to cluster centers
+    for (int i = 1; i <= n; i++) {
+        // continue if node is a cluster center
+        if (cluster[i] == i) continue;
+
+        int adjacent_cluster = false;
+        int closest_cluster = 0;
+        int cluster_dist = INF;
+
+        // check if node is adjacent to any cluster center
+        for (auto u : adj[i]) {
+            if (cluster[u.fr] == u.fr) {
+                adjacent_cluster = true;
+                if (u.sc < cluster_dist) {
+                    closest_cluster = u.fr;
+                    cluster_dist = u.sc;
+                }
+            }
+        }
+
+        if (!adjacent_cluster) {
+            // if node is not adjacent -> add all edges to spanner graph
+
+            for (auto u : adj[i]) {
+                spanner_edges.push_back({i, u.fr, u.sc});
+                adj[u.fr].erase({i, u.sc});
+            }
+
+            adj[i].clear();
+            continue;
+        }
+        
+        // if node is adjacent to any cluster node -> add all edges <= cluster_dist
+        vector<pii> added_edges = {};
+        cluster[i] = closest_cluster;
+
+        for (auto u : adj[i]) {
+            if (u.sc < cluster_dist) {
+                added_edges.push_back(u);
+                spanner_edges.push_back({i, u.fr, u.sc});
+                adj[u.fr].erase({i, u.sc});
+            }
+        }
+
+        for (auto u : added_edges) adj[i].erase(u);
+    }
+    
+    // delete edges between non-sampled vertexes which belong to same cluster
+    for (int i = 1; i <= n; i++) {
+        if (cluster[i] == i) continue;
+
+        vector<pii> delete_edges = {};
+
+        for (auto u : adj[i]) {
+            if (!(cluster[u.fr] == u.fr) && (cluster[u.fr] == cluster[i])) {
+                adj[u.fr].erase({i, u.sc});
+                delete_edges.push_back(u);
+            }
+        }
+
+        for (auto edge : delete_edges) {
+            adj[i].erase(edge);
+        }
+    }
+
+    // Phase 2 : Cluster-Vertex joining
+
+    for (int i = 1; i <= n; i++) {
+        // smallest edge from i to all the clusters
+        vector<pii> smallest_edge(n + 10, {0, INF});
+
+        vector<pii> delete_edges = {}; // edges to be deleted from original graph
+
+        for (auto u : adj[i]) {
+            int cluster_center = cluster[u.fr];
+
+            auto v = smallest_edge[cluster_center];
+
+            if (u.sc < v.sc) {
+                delete_edges.push_back(v);
+                adj[v.fr].erase({i, v.sc});
+                smallest_edge[cluster_center] = u;
+            }
+        }
+
+        for (auto u : delete_edges) adj[i].erase(u);
+
+        for (auto u : adj[i]) {
+            spanner_edges.push_back({i, u.fr, u.sc});
+            adj[u.fr].erase({i, u.sc});
+        }
+
+        adj[i].clear(); // removing all the edges added to spanner graph
+    }
+
+    int total_edges = spanner_edges.size();
+
+    cout << n << " " << total_edges << "\n";
+
+    for (auto edge : spanner_edges) {
+        cout << get<0>(edge) << " " << get<1>(edge) << " " << get<2>(edge) << "\n";
+    }
+}
