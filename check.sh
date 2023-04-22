11aa768f21d644a8320cc9a0ed3bd25e88e35e5f
fine-man <spacetime299@gmail.com>
2023-04-23 00:56:37 +0530

    finished t-spanner.cpp program, added .gitignore and modified check.sh for checking t-spanner

--- check.sh
+++ check.sh
@@ -1,10 +1,12 @@
 # ./check.sh [n-value] [t-value] [cpp src name] [infile] [outfile]
 
-#g++ dgg.cpp -o dgg
-#./dgg $1 > $4
+g++ dgg.cpp -o dgg
+./dgg $1 > $4
+rm ./dgg
 
 g++ $3 -o prog
-./prog < $4 > $5
+./prog $2 < $4 > $5
+rm ./prog
 
 echo $2 > checker_input.txt
 cat $4 >> checker_input.txt
@@ -13,3 +15,4 @@ cat $5 >> checker_input.txt
 g++ checker.cpp -o check
 
 ./check < checker_input.txt
+rm ./check
