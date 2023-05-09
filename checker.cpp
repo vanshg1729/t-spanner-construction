7db3b9a7edf301342a00d515bf63fe5d4e586ce6
fine-man <spacetime299@gmail.com>
2023-05-09 13:04:01 +0530

    added command line argument in checker.cpp and added option for command line checker_args in all test-suite commands

--- checker.cpp
+++ checker.cpp
@@ -34,21 +34,29 @@ void dijkstra(int s, vector<int> & d, vector<int> & p, vector<vector<pair<int, i
     }
 }
 
-void floyd_warshall(int s, vector<vector<int>> & d, vector<vector<pair<int, int>>>& adj) {
+void floyd_warshall(int s, vector<vector<int>> & d, vector<vector<pair<int, int>>>& adj,
+        vector<vector<int>> & path_lengths) {
     int n = adj.size();
     d.assign(n, vector<int>(n, INF));
+    path_lengths.assign(n, vector<int>(n, 0));
+
     for(int i = 0; i < n; i++){
         for(auto next : adj[i]){
             d[i][next.first] = next.second;
             d[next.first][i] = next.second;
+            path_lengths[i][next.first] = 1;
+            path_lengths[next.first][i] = 1;
             // cout<<next.second<<endl;
         }
     }
+
     for (int k = 0; k < n; ++k) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (d[i][k] < INF && d[k][j] < INF)
-                    d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
+                if (d[i][k] < INF && d[k][j] < INF && d[i][k] + d[k][j] < d[i][j]) {
+                    d[i][j] = d[i][k] + d[k][j];
+                    path_lengths[i][j] = path_lengths[i][k] + path_lengths[k][j];
+                }
             }
         }
     }
@@ -75,74 +83,120 @@ void printJson(string a, string b){
     cout<<"\""<<a<<"\":"<<"\""<<b<<"\",";
 }
 
-int main(){
-    int t;
-    cin>>t;
-    int n, m;
-    cin>>n>>m;
+int main(int argc, char *argv[]){
+    bool calculate_distances = false;
+
+    if (argc > 1) {
+        if (atoi(argv[1]) == 1) calculate_distances = true;
+    }
+
     srand(time(0));
+    int t;
+    cin >> t;
+
+    // taking input of the first graph
+    int n, m;
+    cin >> n >> m;
     vector<vector<pair<int,int>>> adj(n, vector<pair<int,int>>());
     
     for(int i = 0; i < m; i++){
         int x, y, w;
-        cin>>x>>y>>w;
-        x--;y--;
-        adj[x].push_back(make_pair(y, w));
-        adj[y].push_back(make_pair(x, w));
+        cin >> x >> y >> w;
+        x--; y--;
+        adj[x].push_back({y, w});
+        adj[y].push_back({x, w});
     }
+
+    // taking input of t-spanner graph of the first graph
     int n2, m2;
-    cin>>n2>>m2;
+    cin >> n2 >> m2;
     vector<vector<pair<int,int>>> adj2(n2, vector<pair<int,int>>());
     for(int i = 0; i < m2; i++){
         int x, y, w;
-        cin>>x>>y>>w;
-        x--;y--;
-        adj2[x].push_back(make_pair(y, w));
-        adj2[y].push_back(make_pair(x, w));
+        cin >> x >> y >> w;
+        x--; y--;
+        adj2[x].push_back({y, w});
+        adj2[y].push_back({x, w});
     }
 
+    // t-spanner stat variables
     int phase1_edge_count = 0, phase2_edge_count = 0;
     double phase1_time = 0, phase2_time = 0, total_time = 0;
+    bool ok = true;
+    double spanner_score = 0;
+
+    // graph stat variables
+    double avg_path_len = 0;
+    int min_path_len = INF, max_path_len = 0;
+    int paths_atleast_t = 0; // number of paths whose length >= k
 
     cin >> phase1_edge_count >> phase2_edge_count;
     cin >> phase1_time >> phase2_time >> total_time;
 
-    vector<vector<int>> dists(n, vector<int>());
-    vector<vector<int>> dists2(n2, vector<int>());
-    // distances(adj, dists);
-    // distances(adj2, dists2);
-    // floyd_warshall(0, dists, adj);
-    // floyd_warshall(0, dists2, adj2);
+    // calculating distances and all the stat variables
+    if (calculate_distances) {
+        vector<vector<int>> dists(n, vector<int>());
+        vector<vector<int>> dists2(n2, vector<int>());
+        vector<vector<int>> path_lengths(n, vector<int>());
+        vector<vector<int>> path_lengths2(n2, vector<int>());
 
-    bool ok = true;
-    double spanner_score = 0;
-    // for(int i = 0; i < n; i++){
-    //     for(int j = 0; j < n; j++){
-    //         spanner_score = max(spanner_score, ((double) dists2[i][j])/dists[i][j]);
-    //         if(dists2[i][j] > t * dists[i][j]){
-	// 	// cout<<i + 1 <<" "<< j + 1 <<" "<<dists[i][j]<<" "<<dists2[i][j]<<endl;
-    //             ok = false;
-    //             break;
-    //         }
-    //     }
-    // }
-/*    for(auto v : dists){
-        for(auto x : v){
-            cout<<x<<" ";
+        //distances(adj, dists);
+        //distances(adj2, dists2);
+        floyd_warshall(0, dists, adj, path_lengths);
+        floyd_warshall(0, dists2, adj2, path_lengths2);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if (i == j) continue;
+                spanner_score = max(spanner_score, ((double) dists2[i][j])/dists[i][j]);
+                min_path_len = min(min_path_len, path_lengths[i][j]);
+                max_path_len = max(max_path_len, path_lengths[i][j]);
+                avg_path_len += path_lengths[i][j];
+
+                if (path_lengths[i][j] >= t) paths_atleast_t++;
+                if(dists2[i][j] > t * dists[i][j]){
+        	// cout<<i + 1 <<" "<< j + 1 <<" "<<dists[i][j]<<" "<<dists2[i][j]<<endl;
+                    ok = false;
+                    break;
+                }
+            }
         }
-        cout<<endl;
+
+        avg_path_len /= n * (n - 1);
+
+    /*    for(auto v : dists){
+            for(auto x : v){
+                cout<<x<<" ";
+            }
+            cout<<endl;
+        }
+
+        for(auto v : dists2){
+            for(auto x : v){
+                cout<<x<<" ";
+            }
+            cout<<endl;
+        }*/
     }
-    for(auto v : dists2){
-        for(auto x : v){
-            cout<<x<<" ";
-        }
-        cout<<endl;
-    }*/
+
+    // print all the stat information
     cout<<"{\"status\":";
-    if(ok)
-        cout<<"\"YES\",";
-    else
-        cout<<"\"NO\",";
+    if(calculate_distances) {
+        if (ok) {
+            cout << "\"YES\",";
+        }
+        else {
+            cout << "\"NO\",";
+        }
+    }
+    else {
+        cout << "\"NA\",";
+    }
+    printJson("calculate_distances", to_string(calculate_distances));
+    printJson("min_path_len", to_string(min_path_len));
+    printJson("avg_path_len", to_string(avg_path_len));
+    printJson("max_path_len", to_string(max_path_len));
+    printJson("paths_atleast_t", to_string(paths_atleast_t));
     printJson("n_value", to_string(n));
     printJson("t_value", to_string(t));
     printJson("spanner_score", to_string(spanner_score));
